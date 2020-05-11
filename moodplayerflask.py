56de06442cdf134ff190059bcba154e2fa853d57
Nick Flores <joseflores2@csumb.edu>
2020-05-11 12:16:50 -0700

    added some json

--- moodplayerflask.py
+++ moodplayerflask.py
@@ -3,12 +3,45 @@
 #CST205
 #May 11, 2020
 
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template 
 
+# create an instance of the Flask class
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
+meagans_faves = [
+	"Chocolaty Coco Puffs",
+	"Hot Cheetos",
+	"Phish the jam band (kinda like Grateful Dead)",
+	"Python programming",
+	"Snapchat Face mod filters"
+]
+
+# route() decorator binds a function to a URL
 @app.route('/')
+def home():
+    my_string = "<h1>Welcome to my page</h1><p>Have a nice day!</p>"
+    return my_string
+
+@app.route('/hello')
 def hello():
-	return render_template('cvwebpage.html')
+    name = saymyname()
+    return '<h1>Hello World</h1><p>Hello world from Flask! Love, ' + name + "</p>"
+
+@app.route('/whoami')
+def whoami():
+    return saymyname()
+
+@app.route('/mytemplate')
+def t_test():
+ return render_template('flask-template.html', var1='1', var2='2')
+
+def saymyname():
+    return "Wes"
+
+@app.route('/template')
+def template():
+	return render_template('cvwebpage.html', name='Josephina Meagan McStallion', faves=meagans_faves, img='meagan.jpg', site='meagan-mcstalion.me.com', p='she', pp='her')
+
+@app.route('/response/<mood>')
+def response_page(mood):
+	return render_template('cvwebpage.html', name='Josephina Meagan McStallion', mood=mood, faves=meagans_faves, img='meagan.jpg', site='meagan-mcstalion.me.com', p='she', pp='her')
